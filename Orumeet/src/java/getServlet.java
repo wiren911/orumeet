f04a421207e221788f0fe667ee570e49c41aa7e0
vonanton <suzack@gmail.com>
2018-02-01 11:25:30 +0100

    Kalender och ny servlet-klass

--- Orumeet/src/java/getServlet.java
+++ Orumeet/src/java/getServlet.java
@@ -1,63 +1,119 @@
-
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+ 
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+ 
 /**
  *
  * @author Anton Söderberg
  */
-public class getServlet extends HttpServlet{
-     Connection conn;
+@WebServlet(urlPatterns = {"/getServlet"})
+public class getServlet extends HttpServlet {
+ 
+    Connection conn;
     Statement stmt;
     String datum;
-    String result;
+ 
     String dburl = "jdbc:mysql://localhost:3306/oru?zeroDateTimeBehavior=convertToNull";
     String Username = "root";
-    String PassWord = "";   
-
-protected void aprocessRequest(HttpServletRequest request, HttpServletResponse response)
+    String PassWord = "";  
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
-            
             datum = request.getParameter("valjDatum");
-            
-            
-            try{
-                Class.forName("com.mysql.jdbc.Driver");
+            /* TODO output your page here. You may use following sample code. */
+           
+            Class.forName("com.mysql.jdbc.Driver");
                 conn = DriverManager.getConnection(dburl, Username, PassWord);
                 stmt = conn.createStatement();
-                String query = "select title, plats from mote where datum ='"+datum+"'";
-                stmt.execute(query);
+                String query = "select * from mote where datum ='"+datum+"'";
+                ResultSet rs = stmt.executeQuery(query);
                
-            }
-            catch (Exception e){
+                while (rs.next()) {
+                String titel = rs.getString("title");
+                String plats = rs.getString("plats");
+               
+               
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Info om möte</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Mötet har titeln: "+titel+", och platsen för mötet är: "+plats+"!</h1>");
+            out.println("</body>");
+            out.println("</html>");
+                }
+                RequestDispatcher rd = request.getRequestDispatcher("index.html");
+            rd.include(request, response);
+        } catch (Exception e){
                 e.printStackTrace();
             }
-            
-        }
     }
 
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        aprocessRequest(request, response);
+        processRequest(request, response);
     }
 
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        aprocessRequest(request, response);
+        processRequest(request, response);
     }
 
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
     @Override
     public String getServletInfo() {
         return "Short description";
-    }
+    }// </editor-fold>
+
 }
